0fb9ab512ad753208f4a4159b6bb095fd191f053
harunbd80 <harun708280@gmail.com>
2025-03-06 23:25:10 +0600

    run server and data base connect

--- index.js
+++ index.js
@@ -1,57 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js"; 
 
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
+dotenv.config();
+connectDB();
 
 const app = express();
-
-const port = process.env.PORT || 9000;
-
-//middleware
 app.use(express.json());
-app.use(cors(
-    {
-        origin: ['http://localhost:5173'], //replace with client address
-        credentials: true,
-    }
-)); 
 
-// cookie parser middleware
-app.use(cookieParser());
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hvhaw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+app.get("/", (req, res) => {
+    res.send("Hello from WorkWave Server");
 });
 
-async function run() {
-  try {
-    
-    await client.connect();
-    
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    
-  }
-}
-run().catch(console.dir);
-
-
-app.get('/', (req, res) => {
-    res.send('Hello from my WorkWave Server')
-})
-
-app.listen(port, () => {
-    console.log('My simple server is running at', port);
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`🚀 Server is running at http://localhost:${PORT}`);
+});
