338a7b037273ef7817dc2ec801d28820d502401e
harunbd80 <harun708280@gmail.com>
2025-03-07 11:26:53 +0600

    succesfuuly registration user

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,12 +4,12 @@ import bcrypt from "bcryptjs"
 
 export const registerUser=async(req,res)=>{
     try{
-        const {name,email,password,photo}=req.body;
+        const {name,email,photo}=req.body;
         const userExit=await User.findOne({email})
         if (userExit)return res.status(400).json({message:'User Already exists'})
         
-        const hashedPassword=await bcrypt.hash(password,10)
-        const user=await User.create({name,email,password:hashedPassword,photo})
+        
+        const user=await User.create({name,email,photo})
         res.status(201).json(user)
 
     }catch{
--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js"; 
-import userRoute from "./routes/userRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
 dotenv.config();
 connectDB();
 
 const app = express();
+app.use(cors({
+  origin: "http://localhost:5174", 
+  credentials: true
+}));
 app.use(express.json());
-app.use('api/users',userRoute)
+app.use('/api/users',userRoutes)
 
 app.get("/", (req, res) => {
     res.send("Hello from WorkWave Server");
