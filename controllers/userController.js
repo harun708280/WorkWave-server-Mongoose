46cd869af9626a59e06b6b422a88c5420a39a3a0
harunbd80 <harun708280@gmail.com>
2025-03-07 12:16:36 +0600

    succesfuuly get single data server

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,3 +18,16 @@ export const registerUser=async(req,res)=>{
     }
 }
 
+export const  getUserByEmail=async(req,res)=>{
+    try{
+        const {email}=req.params;
+        const user=await User.findOne({email})
+        if (!user) return res.status(404).json({message:'user not found'})
+        res.status(200).json(user)
+
+    }catch(error){
+
+        res.status(500).json({ message: error.message });
+
+    }
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { registerUser } from "../controllers/userController.js";
+import { getUserByEmail, registerUser } from "../controllers/userController.js";
 const router=express.Router()
 
 router.post("/",registerUser)
+router.get('/:email',getUserByEmail)
 
 export default router
