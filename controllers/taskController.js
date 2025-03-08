9a2bc7c4a0f7bc1d9244e78ac2bf5e4ef42cefe7
harunbd80 <harun708280@gmail.com>
2025-03-08 13:27:00 +0600

    udated task status api

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -28,3 +28,18 @@ export const getAllTask=async(req,res)=>{
     }
 
 }
+
+export const updateTaskStatus=async(req,res)=>{
+    try{
+        const {taskId}=req.params;
+        const {status}=req.body
+        const updatedTask=await Tasks.findByIdAndUpdate(
+            taskId,{status},{new:true}
+        )
+        if (!updatedTask) return res.status(404).json({ message: "Task not found" });
+
+        res.status(200).json(updatedTask);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createTask, getAllTask } from "../controllers/taskController.js";
+import { createTask, getAllTask, updateTaskStatus } from "../controllers/taskController.js";
 
 const router=express.Router()
 
 router.post('/',createTask)
 router.get('/',getAllTask)
+router.put("/:taskId/status",updateTaskStatus)
 
 
 export default router
